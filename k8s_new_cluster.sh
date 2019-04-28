d99b2c2cddaa5fe13f663af19b33d5d811fe69d4
System Administrator <root@Serges-MacBook-Air.local>
2019-04-27 18:45:09 -0700

    adding regex to extract ip

--- k8s_new_cluster.sh
+++ k8s_new_cluster.sh
@@ -3,17 +3,13 @@
 #install dependencies on all k8s
 sudo docker run --rm -it -e ANSIBLE_HOST_KEY_CHECKING=false -v $(pwd):/ansible/playbooks ansible-local k8s-dependencies.yml -i inventory -K --ask-pass
 
-sleep 120
-
 #deploy master node
 sudo docker run --rm -it -e ANSIBLE_HOST_KEY_CHECKING=false -v $(pwd):/ansible/playbooks ansible-local k8s-master.yml -vvv  -i inventory -K --ask-pass
 
-sleep 120
+sleep 180
 
 #join master node
 sudo docker run --rm -it -e ANSIBLE_HOST_KEY_CHECKING=false -v $(pwd):/ansible/playbooks ansible-local k8s-node.yml -vvv  -i inventory -K --ask-pass
-
-read -n1 -r -p "Please copy the name in /etc/hostname to /etc/hosts..." key
 
 #deploy jenkin master
 scp -rp ../charts/* ubuntu@$1:/home/ubuntu/
